cc487170c94e26d6ee1330fd992b87e0a511a0e6
oatmeal3000 <oatmeal3000@163.com>
2015-08-12 10:30:44 +0800

    process related

---
+++ Thread_Process/Process/fork.py
@@ -0,0 +1,13 @@
+"""a basic fork in action"""
+
+import os
+
+def my_fork():
+    child_pid = os.fork()
+    if child_pid == 0:
+        print "Child Process: PID# %s" % os.getpid()
+    else:
+        print "Parent Process: PID# %s" % os.getpid()
+
+if __name__ == "__main__":
+    my_fork()
---
+++ Thread_Process/Process/upper.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int ch;
+    while((ch = getchar()) != EOF){
+        putchar(toupper(ch));
+    }
+    return 0;
+}
